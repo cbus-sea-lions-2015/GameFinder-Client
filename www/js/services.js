1db42f4754de1512e507dd267cda7e72f0f96421
Daniel Martinez <mikedmtz@gmail.com>
2015-04-19 13:02:42 -0400

    modify search service

--- www/js/services.js
+++ www/js/services.js
@@ -1,13 +1,26 @@
 angular.module('gameFinder.services', [])
 
   .factory('SearchService', function($http) {
+    var cachedGames;
     return {
+
         findAll: function() {
-            return $http.get("http://localhost:3000/games").success(function(response){
+            return cachedGames || $http.get("http://localhost:3000/games").success(function(response){
+                cachedGames = response;
                 return response;
                 // return games;
             });
         },
+
+        findByName: function(gameName) {
+            // var url = ["http://localhost:3000/games/", gameId].join("")
+            // var url = $http.get("http://localhost:3000/games")
+
+            return $http.get(url).then(function(response){
+                game = response;
+                return game;
+            });
+        }
 
         findById: function(gameId) {
             var url = ["http://localhost:3000/games/", gameId].join("")
