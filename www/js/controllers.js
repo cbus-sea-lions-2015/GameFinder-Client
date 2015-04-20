07a2e5d7b2a16f963f7ac2c8c04ed766fad1657f
Daniel Martinez <mikedmtz@gmail.com>
2015-04-19 20:42:50 -0400

    remove libctrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -37,17 +37,6 @@ angular.module('gameFinder.controllers', [])
     };
   })
 
-  .controller('LibCtrl', function($scope, $http, $stateParams) {
-    var url = 'http://gamefinder.herokuapp.com/libraries';
-
-    $http.get(url).success(function(game) {
-      $scope.game = game;
-      console.log('success!');
-    }).error(function(data) {
-      console.log('server side error occurred.');
-    });
-  })
-
   .controller('GameCtrl', function($scope, $http, $stateParams) {
     var url = ['http://gamefinder.herokuapp.com/games/',$stateParams.gameId].join("");
 
@@ -58,7 +47,6 @@ angular.module('gameFinder.controllers', [])
       console.log('server side error occurred.');
     });
   })
-
 
   .controller('SearchCtrl', function($scope, GameService) {
 
